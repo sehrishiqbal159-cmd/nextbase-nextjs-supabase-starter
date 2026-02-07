b907fcadc4ecb6d071a684e08ddfb9c61460922a
Sehrish Iqbal <sehrishiqbal159-cmd@gmail.com>
2026-02-07 18:06:42 +0500

    Initial commit

---
+++ src/app/(dynamic-pages)/layout.tsx
@@ -0,0 +1,17 @@
+
+
+export const metadata = {
+  icons: {
+    icon: '/images/logo-black-main.ico',
+  },
+  title: 'Nextbase Open source',
+  description: 'Nextbase Open source',
+};
+
+export default async function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <>{children}</>;
+}
